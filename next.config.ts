0a2e5fafbc5ec481da31e08ce5635f4b92b464c6
kikicreativeans <kiki@creativeans.com>
2025-02-07 13:29:21 +0700

    initial next js github page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: '/qhyabdoel/',
+  basePath: '/qhyabdoel',
+  output: 'export'
 };
 
 export default nextConfig;
