8edcea2abea14f719fe283dccb38b0ba0e9935a8
kikicreativeans <kiki@creativeans.com>
2025-11-21 10:40:38 +0700

    add github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,12 @@ export default function Home() {
       <nav className="w-full mb-12 md:flex justify-between items-center">
         <div className="text-2xl font-bold md:mb-0 mb-4">Kiki Abdulloh</div>
         <div>
-          <a href="#projects" className="md:mx-4 text-lg">
-            Repositories
+          <a
+            href="https://github.com/qhyabdoel"
+            target="_blank"
+            className="md:mx-4 text-lg"
+          >
+            GitHub
           </a>
         </div>
       </nav>
