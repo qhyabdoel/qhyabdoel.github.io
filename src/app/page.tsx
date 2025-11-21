b9c4e57b08350c790d9e0b4938d6cd1b926003d9
kikicreativeans <kiki@creativeans.com>
2025-11-21 10:34:21 +0700

    update config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <p>Hi, my name is Kiki Abdulloh.</p>;
+  return (
+    <main className="flex min-h-screen flex-col items-center p-12 md:p-24">
+      {/* Header */}
+      <nav className="w-full mb-12 md:flex justify-between items-center">
+        <div className="text-2xl font-bold md:mb-0 mb-4">Kiki Abdulloh</div>
+        <div>
+          <a href="#projects" className="md:mx-4 text-lg">
+            Repositories
+          </a>
+        </div>
+      </nav>
+    </main>
+  );
 }
